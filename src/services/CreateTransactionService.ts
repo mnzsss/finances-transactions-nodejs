ee45e7a187301159650c888e8f6d4ed4511bb3d9
Gabriel Menezes <gabrielsmenezes99@gmail.com>
2020-04-18 20:47:11 -0300

    feat: validate transaction type

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Transaction type is not valid.');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
